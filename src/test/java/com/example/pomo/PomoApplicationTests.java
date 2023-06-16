d7bb80344083338be2133ad28978ebbc44988ddd
spaesk <spaesk@gmail.com>
2023-06-16 23:58:55 +0530

    Add greetings controller

---
+++ src/test/java/com/example/pomo/PomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
